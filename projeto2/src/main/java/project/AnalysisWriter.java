dd59d06ea060289eb15fbf853ca27ecb18a37e8e
RafaellaJunqueira <rafaellafjunqueira@gmail.com>
2021-10-31 16:13:49 -0300

    [Refact] Extrair Método Origem: AnalysisWriter.java Impacto: Classes: AnalysisWriter.java Métodos: writeFile(String[] parseList, int fileFormat, String filePath, String fileName), writeOnFile(String filePath, String fileName, int fileFormat, String[] parseList), canWriteOnFile(String filePath) Explicação: A refatoração foi aplicada no método writeFile() da classe AnalysisWriter. A instanciação do objeto de leitura de arquivo, BufferedWriter, e a instanciação do objeto necessário para essa leitura, File, foram movidos para um método separado chamado writeOnFile(), assim como a função de escrita de volta no arquivo da transformação dos dados, também foi movida para um novo método chamado canWriteOnFile a instanciação de um objeto File onde é verificado se o destino do novo arquivo a ser escrito está disponível para escrita, (I.E. se o usuário utilizando a aplicação pode escrever no diretório).
    Co-authored-by: GabrielHussein <gabrielhussein83@gmail.com>

--- projeto2/src/main/java/project/AnalysisWriter.java
+++ projeto2/src/main/java/project/AnalysisWriter.java
@@ -1,4 +1,5 @@
 package main.java.project;
+
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
@@ -7,71 +8,76 @@ import java.io.IOException;
 import main.java.project.exception.EscritaNaoPermitidaException;
 
 public class AnalysisWriter {
-    private boolean writeSuccess;
+	private boolean writeSuccess;
+	private String singleLine = "";
+	private int parseListLength;
+	private String[] arrayAux;
+	private int arraySize;
+	private int lineCounter = 0;
 
-    public AnalysisWriter(String[] parseList, int inputFileFormat, String inputFilePath, String fileName) throws EscritaNaoPermitidaException, IOException{
-        this.writeFile(parseList, inputFileFormat, inputFilePath, fileName);
-    }
+	public AnalysisWriter(String[] parseList, int inputFileFormat, String inputFilePath, String fileName)
+			throws EscritaNaoPermitidaException, IOException {
+		this.writeFile(parseList, inputFileFormat, inputFilePath, fileName);
+	}
 
-    public void writeFile(String[] parseList, int fileFormat, String filePath, String fileName) throws EscritaNaoPermitidaException, IOException{
-        File outFile = new File(filePath);
-        fileName = fileName.replace(".out", "");
-        fileName = fileName + "Tab.out";
-        if(outFile.canWrite()){
-            /*
-            ajuste de formato
-            parseList
-            filePath invalido
-            */
-            int lineCounter = 0;
-            String singleLine = "";
-            File completeFile = new File(filePath, fileName);
-            BufferedWriter writer = new BufferedWriter(new FileWriter(completeFile));
-            
-            if (fileFormat == 0) {
-            	int parseListLength = parseList.length;
-            	
-            	String[] arrayAux = parseList[0].split(";");
-            	int arraySize = arrayAux.length;
-            	
-            	int counter = 0;
-            	
-            	for (int k = 0; k < parseListLength; k++) {
-            		singleLine += counter + ";";
-            		counter ++;
-            	}
-            	
-            	writer.write(singleLine);
-            	writer.newLine();
-            	
-            	for (int i = 1; i < arraySize; i++) {
-            		singleLine = "";
-            		for (int j = 0; j < parseListLength; j++) {
-            			singleLine += parseList[j].split(";")[i] + ";";
-            		}
-            		writer.write(singleLine);
-            		writer.newLine();
-            	}
-            	
-            	setWriteSuccess(true);
-            	writer.close();
-            	
-            } else {
-            	for(String line : parseList){
-            		singleLine = lineCounter + line;
-            		writer.write(singleLine);
-            		writer.newLine();
-            		lineCounter++;	
-            	}
-            	setWriteSuccess(true);
-            	writer.close();
-            }
-            
-        } else {
-            setWriteSuccess(false);
-            throw new EscritaNaoPermitidaException();
-        }
-    }
+	public void writeFile(String[] parseList, int fileFormat, String filePath, String fileName)
+			throws EscritaNaoPermitidaException, IOException {
+		fileName = fileName.replace(".out", "");
+		fileName = fileName + "Tab.out";
+		if (canWriteOnFile(filePath)) {
+			parseListLength = parseList.length;
+
+			arrayAux = parseList[0].split(";");
+			arraySize = arrayAux.length;
+
+			int counter = 0;
+
+			for (int k = 0; k < parseListLength; k++) {
+				singleLine += counter + ";";
+				counter++;
+			}
+			setWriteSuccess(writeOnFile(filePath, fileName, fileFormat, parseList));
+		} else {
+			setWriteSuccess(false);
+			throw new EscritaNaoPermitidaException();
+		}
+	}
+
+	private boolean canWriteOnFile(String filePath) {
+		File outFile = new File(filePath);
+		if(outFile.canWrite()){
+			return true;
+		}
+		return false;
+	}
+
+	private boolean writeOnFile(String filePath, String fileName, int fileFormat, String[] parseList)
+			throws IOException {
+		File completeFile = new File(filePath, fileName);
+		BufferedWriter writer = new BufferedWriter(new FileWriter(completeFile));
+		if (fileFormat == 0) {
+			writer.write(singleLine);
+			writer.newLine();
+			for (int i = 1; i < arraySize; i++) {
+				singleLine = "";
+				for (int j = 0; j < parseListLength; j++) {
+					singleLine += parseList[j].split(";")[i] + ";";
+				}
+				writer.write(singleLine);
+				writer.newLine();
+			}
+			writer.close();
+		} else {
+			for (String line : parseList) {
+				singleLine = lineCounter + line;
+				writer.write(singleLine);
+				writer.newLine();
+				lineCounter++;
+			}
+			writer.close();
+		}
+		return true;
+	}
 
 	public boolean isWriteSuccess() {
 		return writeSuccess;
